c6b51213fe4d7710138e4c886a79f204eef4c3f3
Adrian Elton-Browning <adrian.browning@tealium.com>
2024-07-02 15:40:45 +0100

    issue-12 fix: updated schema to match what we are testing against. fix: lastModifiedBy -> lastModifiedById fix: where DISTINCT is rendered feat: select * from user feat: select * from user JOIN Post feat: selectAll from User feat: selectAll from User JOIN Post

--- src/db-select.ts
+++ src/db-select.ts
@@ -21,7 +21,7 @@ const DB = {
             content: "?string",
             published: "boolean",
             authorId: "number",
-            lastModifiedBy: "number",
+            lastModifiedById: "number",
         },
         //safeJoin
         relations: {
@@ -87,7 +87,7 @@ type GetTSType<str extends string> = str extends `string` ? string
                 : "Unknown type";
 
 
-type ValidSelect = "*" | Array<string>; // TODO
+type ValidSelect = "*" ; // TODO
 
 type TTables = DATABASE["table"];
 
@@ -103,22 +103,25 @@ export class DbSelect {
 
 }
 
+type Values = {
+    database: TTables,
+    selectDistinct?: true;
+    selects: Array<string>;
+    tables?: Array<{table: TTables, local: string, remote: string}>;
+    limit?: number;
+    offset?: number;
+    where?: Array<{ col: string; operator: string; value: unknown }>;
+    having?: Array<{ col: string; operator: string; value: unknown }>;
+    groupBy?: Array<string>;
+    orderBy?: Array<`${string}} ${"DESC" | "ASC"}`>;
+};
+
 /*
 run
  */
-class _fRun<TDBBase extends TTables> {
+class _fRun<TDBBase extends TTables, TJoins extends Array<TTables> = [], TSelectRT extends Record<string, any> = {}> {
     constructor(protected db: PrismaClient,
-                protected values: {
-                    database: string,
-                    selects: Array<string>;
-                    tables?: Array<string>;
-                    limit?: number;
-                    offset?: number;
-                    where?: Array<{ col: string; operator: string; value: unknown }>;
-                    having?: Array<{ col: string; operator: string; value: unknown }>;
-                    groupBy?: Array<string>;
-                    orderBy?: Array<`${string}} ${"DESC" | "ASC"}`>;
-                }) {
+                protected values: Values) {
         this.values.limit = typeof this.values.limit === "number" ? this.values.limit : undefined;
         this.values.offset = typeof this.values.offset === "number" ? this.values.offset : undefined;
     }
@@ -126,13 +129,18 @@ class _fRun<TDBBase extends TTables> {
     run() {
         return this.db.$queryRawUnsafe(
             this.getSQL()
-        ) as unknown as Prisma.PrismaPromise<Array<Extract<DATABASE, { table: TDBBase }>["fields"]>>;
+        ) as unknown as Prisma.PrismaPromise<Array<TSelectRT>>
+        //as Prisma.PrismaPromise<Array<Extract<DATABASE, { table: TDBBase }>["fields"]>>;
+    }
+    getTables(){
+        return {} as [TDBBase, ...TJoins];
     }
     getSQL() {
+
         return [
-            `select ${this.values.selects.join(', ')}`,
+            "select " + (this.values.selectDistinct === true ? "DISTINCT " : "") + this.values.selects.join(', '),
             `from ${this.values.database}`,
-            this.values.tables?.join(" ") ?? "",
+            this.values.tables?.map(({table, local, remote}) => `JOIN ${table} ON ${local} = ${remote}`).join(" ") ?? "",
             this.values.limit === undefined ? "" : `limit ${this.values.limit}`,
             this.values.offset === undefined ? "" : `offset ${this.values.offset}`].join(" ").trim()+ ";";
     }
@@ -143,9 +151,9 @@ OFFSET -
 run
 */
 
-class _fOffset<TDBBase extends TTables> extends _fRun<TDBBase> {
+class _fOffset<TDBBase extends TTables, TJoins extends Array<TTables> = [],TSelectRT extends Record<string, any>={}> extends _fRun<TDBBase, TJoins, TSelectRT> {
     offset(offset: number) {
-        return new _fRun<TDBBase>(this.db, {...this.values, offset});
+        return new _fRun<TDBBase,TJoins, TSelectRT>(this.db, {...this.values, offset});
     }
 }
 
@@ -155,9 +163,9 @@ OFFSET -
 run
 */
 
-class _fLimit<TDBBase extends TTables> extends _fOffset<TDBBase> {
+class _fLimit<TDBBase extends TTables, TJoins extends Array<TTables> = [], TSelectRT extends Record<string, any> = {}> extends _fOffset<TDBBase,TJoins, TSelectRT> {
     limit(limit: number) {
-        return new _fRun(this.db, {...this.values, limit});
+        return new _fRun<TDBBase, TJoins, TSelectRT>(this.db, {...this.values, limit});
     }
 }
 
@@ -168,9 +176,9 @@ LIMIT - the returned data is limited to row count.
 OFFSET -
 run
 */
-class _fOrderBy<TDBBase extends TTables> extends _fLimit<TDBBase> {
+class _fOrderBy<TDBBase extends TTables, TJoins extends Array<TTables> = [], TSelectRT extends Record<string, any> = {}> extends _fLimit<TDBBase, TJoins, TSelectRT> {
     orderBy(orderBy: Array<`${string}} ${"DESC" | "ASC"}`>) {
-        return new _fLimit<TDBBase>(this.db, {...this.values, orderBy});
+        return new _fLimit<TDBBase, TJoins, TSelectRT>(this.db, {...this.values, orderBy});
     }
 }
 
@@ -183,15 +191,62 @@ OFFSET -
 run
 */
 
-class _fSelect<TDBBase extends TTables> extends _fOrderBy<TDBBase> {
-    select(select: ValidSelect) {
-        return new _fOrderBy<TDBBase>(this.db, {...this.values, selects: Array.isArray(select) ? select : [select]});
+// type MergeItems<T, Tables extends Array<TTables>, IncTName extends boolean = false> = T extends "*" ? Prettify<IterateTables<Tables, IncTName>> : never;
+// type IterateTables<Tables extends Array<TTables>, IncTName extends boolean, acc extends Record<string, any> = {}> =
+//     Tables extends [infer T extends TTables, ...infer Rest extends Array<TTables>]
+//     ? IterateTables<Rest, IncTName, acc & IncTName extends false
+//             ? Extract<DATABASE, { table: T }>["fields"]
+//             : never/*{
+//                [f in keyof Extract<DATABASE, { table: T }>["fields"] as `${T}.${IsString<f>}`]: Extract<DATABASE, { table: T }>["fields"][f]
+//                 }*/
+//             >
+//     : acc;
+
+
+type MergeItems<T, Tables extends Array<TTables>, IncTName extends boolean = false> = T extends "*" ? Prettify<IterateTables<Tables, IncTName>> : never;
+type IterateTables<Tables extends Array<TTables>, IncTName extends boolean, acc extends Record<string, any> = {}> =
+    Tables extends [infer T extends TTables, ...infer Rest extends Array<TTables>]
+        ? [IncTName] extends [false]
+            ? IterateTables<Rest, IncTName, acc &  GetFieldsFromTable<T>>
+            : IterateTables<Rest, IncTName, acc &  IterateFieldsOfTable<T>>
+        : acc;
+
+type IterateFieldsOfTable<T extends TTables, Fields = Extract<DATABASE, { table: T }>["fields"]> = {
+    [f in keyof Fields as `${T}.${IsString<f>}`]: Fields[f]
+}
+
+class _fSelect<TDBBase extends TTables, TJoins extends Array<TTables> = [], TSelectRT extends Record<string, any> = {}> extends _fOrderBy<TDBBase,TJoins,TSelectRT> {
+    select<TSelect extends ValidSelect>(select: TSelect) {
+        /*TODO
+            if "", omit any previous with same name, add new ones
+        */
+
+        return new _fSelect<TDBBase,TJoins,TSelectRT & MergeItems<TSelect,[TDBBase, ...TJoins]>>(this.db, {...this.values, selects:  [...this.values.selects, select] });
     }
 }
 
-class _fSelectDistinct<TDBBase extends TTables> extends _fSelect<TDBBase> {
+class _fSelectDistinct<TDBBase extends TTables, TJoins extends Array<TTables> = [], TSelectRT extends Record<string, any>= {}> extends _fSelect<TDBBase,TJoins,TSelectRT> {
     selectDistinct() {
-        return new _fSelect<TDBBase>(this.db, {...this.values, selects: ["DISTINCT"]});
+        return new _fSelect<TDBBase, TJoins,TSelectRT>(this.db, {...this.values, selectDistinct: true});
+    }
+    selectAll() {
+        //TODO
+        //Need to loop through DATABASE object
+        //if 1 table, no prefix
+        // if more prefix with table name
+
+
+        const selects = (function(values: Values){
+            if (values.tables && values.tables.length > 0) {
+                return [values.database, ...values.tables.map(t => t.table)].reduce<Array<string>>((acc, table): Array<string> => {
+                    return acc.concat(Object.keys(DB[table].fields).map((field) => `${table}.${field} AS \`${table}.${field}\``))
+                }, []);
+            }
+
+            return Object.keys(DB[values.database].fields);
+        }(this.values))
+
+        return new _fOrderBy<TDBBase,TJoins,MergeItems<"*", [TDBBase, ...TJoins] ,TJoins extends [TTables,...Array<TTables> ] ? true: false>>(this.db, {...this.values, selects });
     }
 }
 
@@ -204,9 +259,9 @@ LIMIT - the returned data is limited to row count.
 OFFSET -
 */
 
-class _fHaving<TDBBase extends TTables> extends _fSelectDistinct<TDBBase> {
+class _fHaving<TDBBase extends TTables, TJoins extends Array<TTables> = []> extends _fSelectDistinct<TDBBase,TJoins> {
     having(col: string, operator: string, value: unknown) {
-        return new _fHaving<TDBBase>(this.db, {
+        return new _fHaving<TDBBase,TJoins>(this.db, {
             ...this.values,
             having: [...this.values.having || [], {col, operator, value}]
         });
@@ -222,10 +277,10 @@ LIMIT - the returned data is limited to row count.
 OFFSET -
 */
 
-class _fGroupBy<TDBBase extends TTables> extends _fHaving<TDBBase> {
+class _fGroupBy<TDBBase extends TTables, TJoins extends Array<TTables> = []> extends _fHaving<TDBBase,TJoins> {
 
     groupBy(groupBy: Array<string>) {
-        return new _fHaving<TDBBase>(this.db, {...this.values, groupBy: groupBy});
+        return new _fHaving<TDBBase,TJoins>(this.db, {...this.values, groupBy: groupBy});
     }
 
     /**
@@ -248,7 +303,7 @@ LIMIT - the returned data is limited to row count.
 OFFSET -
 */
 
-class _fWhere<TDBBase extends TTables> extends _fGroupBy<TDBBase> {
+class _fWhere<TDBBase extends TTables, TJoins extends Array<TTables> = []> extends _fGroupBy<TDBBase, TJoins> {
 
     //TODO
     // Not null - remove null from type
@@ -259,7 +314,7 @@ class _fWhere<TDBBase extends TTables> extends _fGroupBy<TDBBase> {
 
 
     where(col: string, operator: string, value: unknown) {
-        return new _fWhere<TDBBase>(this.db, {
+        return new _fWhere<TDBBase, TJoins>(this.db, {
             ...this.values,
             where: [...this.values.where || [], {col, operator, value}],
         });
@@ -278,7 +333,8 @@ OFFSET -
 */
 
 
-type GetColsFromTable<TDBBase extends TTables> = keyof Extract<DATABASE, { table: TDBBase }>["fields"];
+type GetFieldsFromTable<TDBBase extends TTables> =  Extract<DATABASE, { table: TDBBase }>["fields"];
+type GetColsFromTable<TDBBase extends TTables> = keyof GetFieldsFromTable<TDBBase>;
 type GetJoinCols<TDBBase extends TTables> = TDBBase extends any ? IterateFields<TDBBase, IsString<GetColsFromTable<TDBBase>>> : never;
 type IterateFields<TDBBase extends TTables, F extends string> = `${TDBBase}.${F}`;
 
@@ -302,9 +358,11 @@ export type GetUnionOfRelations<TSafe> = {
             T extends string ? TSafe[T][TLocal] extends Array<string> ? ArrayToString<TSafe[T][TLocal][number], T> : never : never
     ]
     }[keyof TSafe[T]];
-}[keyof TSafe]
+}[keyof TSafe];
 
-export type find<T extends [string, string], toFind extends string> = T extends [infer col1, infer col2] ? col1 extends toFind ? col2 : never : never;
+type ValidStringTuple<T> = T extends [string, string] ? T : never;
+
+type find<T extends [string, string], toFind extends string> = T extends [infer col1, infer col2] ? col1 extends toFind ? col2 : never : never;
 
 // type CleanUpFromNames<TFromTable extends TTables, TCols extends string> = TCols extends `${TFromTable}.${infer Col}` ? Col : TCols;
 
@@ -341,7 +399,7 @@ type GetColumnType<Table extends TTables, Col1 extends keyof _db[Table]["fields"
 
 type GetJoinOnColsType<Type extends string, TDBBase extends TTables, TJoins extends Array<TTables>> =
     // GetColsFromTableType<TDBBase, Type>
-    GetJoinColsType<[TDBBase,...TJoins][number],Type>;
+    GetJoinColsType<[TDBBase, ...TJoins][number],Type>;
 
 type GetColsFromTableType<TDBBase extends TTables, Type extends string> =
     //@ts-expect-error Try and come back to
@@ -352,19 +410,17 @@ type Loop<Keys extends string, Type extends string>=  Keys extends Type ? Type :
 type GetJoinColsType<TDBBase extends TTables, Type extends string> = IterateFields<TDBBase, IsString<GetColsFromTableType<TDBBase, Type>>>;//, Type];
 
 
-class _fJoin<TDBBase extends TTables, TJoins extends Array<TTables> = []> extends _fWhere<TDBBase> {
+class _fJoin<TDBBase extends TTables, TJoins extends Array<TTables> = []> extends _fWhere<TDBBase, TJoins> {
 
     join<Table extends TTables,
-        //@ts-expect-error this does work TODO comeback to
-        TJoinCols extends [string, string] = GetUnionOfRelations<SafeJoins<Table, [TDBBase, ...TJoins]>>,
+        TJoinCols extends [string, string] = ValidStringTuple<GetUnionOfRelations<SafeJoins<Table, [TDBBase, ...TJoins]>>>,
         TCol1 extends TJoinCols[0] = never
     >(table: Table, field: TCol1, reference: find<TJoinCols, TCol1> ) { //CleanUpFromNames<TDBBase, find<TJoinCols, TCol1>>
         return new _fJoin<TDBBase, [...TJoins, Table]>(this.db, {
             ...this.values,
-            tables: [...this.values.tables || [], `join ${table} ${String(field)} on ${this.values.database}.${String(reference)}`]
+            tables: [...this.values.tables || [], {table, local: field, remote: reference}]
         });
     }
-
 
     joinUnsafe<Table extends TTables,
         TCol1 extends GetColsFromTable<Table>,
@@ -375,7 +431,7 @@ class _fJoin<TDBBase extends TTables, TJoins extends Array<TTables> = []> extend
         >(table: Table, field: TCol1, reference: TCol2) {
         return new _fJoin<TDBBase, [...TJoins, Table]>(this.db, {
             ...this.values,
-            tables: [...this.values.tables || [], `join ${table} ${table}.${String(field)} on ${String(reference)}`]
+            tables: [...this.values.tables || [], {table, local: `${table}.${String(field)}`, remote: reference} ]
         });
     }
 
@@ -383,10 +439,9 @@ class _fJoin<TDBBase extends TTables, TJoins extends Array<TTables> = []> extend
         TCol2 extends GetJoinCols< [...TJoins, TDBBase][number]> >(table: Table, field: GetColsFromTable<Table>, reference: TCol2) {
         return new _fJoin<TDBBase, [...TJoins, Table]>(this.db, {
             ...this.values,
-            tables: [...this.values.tables || [], `join ${table} ${table}.${String(field)} on ${String(reference)}`]
+            tables: [...this.values.tables || [], {table, local: `${table}.${String(field)}`, remote: reference} ]
         });
     }
-
 
     // innerJoin(table: TTables, col1:string, col2:string){
     //     return new _fJoin<TDBBase>(this.db, {...this.values, tables: [...this.values.tables || [], table]});
