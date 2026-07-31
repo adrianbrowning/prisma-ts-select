7312ad46da7b8b5c03e8bd692178e4ac754bccdb
Adrian Elton-Browning <adrian.browning@tealium.com>
2026-07-31 16:07:57 +0100

    fix: Lint errors

--- packages/prisma-ts-select/tests/generator.spec.ts
+++ packages/prisma-ts-select/tests/generator.spec.ts
@@ -56,8 +56,9 @@ void describe("Generator package.json output", () => {
 
   void test("exports shape", () => {
     const { exports } = readPkgJson("usage-sqlite-v7");
-     
-    for (const entry of Object.values(exports)) {
+
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
+    for (const entry of Object.values(exports) as Array<Record<string, string>>) {
       assert.strictEqual(typeof entry.types, "string");
       assert.strictEqual(typeof entry.import, "string");
     }
